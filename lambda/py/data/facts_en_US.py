e5f43f18a35e687d647d117927d837c20df7bea7
Venu Vardhan Reddy Tekula <venuvardhanreddytekula8@gmail.com>
2019-01-16 03:26:56 +0530

    added quotes data

---
+++ lambda/py/data/facts_en_US.py
@@ -0,0 +1,21 @@
+data = [
+  'Our highest, most important duty in this world is to help our fellow beings.',
+  'We should serve others without any expectations whatsoever. When others throw thorns at us, we should be able to throw flowers back at them.',
+  'Most people are concerned only with what they can get from the world, but it is what we are able to give to others that determines the quality of our life.',
+  'What the world needs are servants, not leaders. Everyone’s wish is to become a leader….Let us become a real servant instead.',
+  'Go out and serve the suffering. Learn to place others in front of yourself.',
+  'Don\'t be discouraged by your incapacity to dispel darkness from the world. Light your little candle and step forward.',
+  'Just as the right hand comes to dress the wound on the left hand, we should see another person\'s sorrows as our own and come to his or her aid.',
+  'Nowadays everybody wants to become a leader. No one wants to become a servant. In reality, the world is badly in need of servants, not leaders. A real servant is a real leader.',
+  'One should see any opportunity to serve as a rare and precious gift...and never waste such an opportunity.',
+  'As you perform good actions selflessly, true love will blossom, which will purify our emotional mind.',
+  'In this age of selfishness, selfless service is the only soap that truly purifies.',
+  'The beauty and charm of selfless love and service should not die away from the face of this earth. The world should know that a life of dedication is possible, that a life inspired by love and service to humanity is possible.',
+  'Serving the world with love and cooperation, you will find your own true self. As you help those in need, selfishness will fall away, and without even noticing you will find your own fulfillment.',
+]
+
+SKILL_NAME = "Amma Teachings"
+GET_FACT_MESSAGE = "Here's your Teaching: "
+HELP_MESSAGE = "You can say exit... What can I help you with?"
+HELP_REPROMPT = "What can I help you with?"
+STOP_MESSAGE = "Namah Shivaya!"
